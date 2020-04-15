25c7d2bee51585ece14091bbafae14b942f40476
Herb1691 <hwright_1691@hotmail.com>
2020-04-15 17:03:20 -0500

    Added ConfirmationModal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { ITodo } from './interfaces/itodo';
+import { TodoService } from './services/todo.service';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,12 @@ export class AppComponent implements OnInit{
   title = 'Todos';
   todoTitle: string;
   todoDescription: string;
+  todoList: Array<ITodo>;
+
+  constructor(private todoService: TodoService) {}
 
   ngOnInit() {
+    this.todoList = this.todoService.getTodos();
   }
 
     // resets our todoTitle variable to an empty string
